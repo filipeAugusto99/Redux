589b6bf7269d07f8e6a8ea231473036454a34c64
Filipe Augusto <filipecordeiro213@gmail.com>
2023-03-24 14:28:57 -0300

    Redux - part II

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
-import { useSelector } from 'react-redux'
+import { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
 function App() {
-  const tasks = useSelector(state => state.data)
+  const [inputTask, setInputTask] = useState()
+
+  const tasks = useSelector(state => state.tasks)
+  const dispatch = useDispatch()
+
+  const handleInput = e => {
+    setInputTask(e.target.value)
+  }
+
+  const handleSubmit = () => {
+    dispatch({
+      type: 'ADD_TASK',
+      payload: inputTask,
+    })
+  }
+
+  const handleRemove = task => {
+    dispatch({
+      type: 'REMOVE_TASK',
+      payload: task
+    })
+  }
 
   return (
-    <ul>
-      {
-        tasks.map(task => <li>{task}</li>)
-      }
-    </ul>
+    <>
+      <ul>
+        {
+          tasks.map(task => 
+            <li key={task}>
+              {task}
+              <button onClick={() => handleRemove(task)}>Remover</button>
+            </li>
+          )
+        }
+      </ul>
+      <input type="text" onChange={handleInput} value={inputTask} />
+      <button onClick={handleSubmit}> Inserir </button>
+    </>
   )
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,29 @@
 import { createStore } from 'redux'
 
-const  INITIAL_STATE = {
-  data: []
+const INITIAL_STATE = {
+  tasks: [
+    'Estudar React.js'
+  ]
 }
 
 // REDUCER
-function todo(state, action) {
+function todo(state = INITIAL_STATE, action) {
   switch(action.type) {
     case 'ADD_TASK': 
       return {
         ...state,
-        data: [
-          ...state.data,
+        tasks: [
+          ...state.tasks,
           action.payload
         ]
       }
+
+    case 'REMOVE_TASK':
+      const newState = state.tasks.filter(task => task !== action.payload)
+      return {
+        tasks: newState
+      }
+
     default:
       return state
   }
